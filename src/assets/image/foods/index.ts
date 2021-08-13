7e2a6c7540df8c1cbb051be620aca623cb4440dc
HeMiaoQ <he_miaoqing@163.com>
2021-08-13 16:12:01 +0800

    docs：删除版权 声明

--- src/assets/image/foods/index.ts
+++ src/assets/image/foods/index.ts
@@ -1,14 +1,4 @@
-/*
-* Copyright (C) 2011-2019 ShenZhen iBOXCHAIN Information Technology Co.,Ltd.
-* All right reserved.
-*
-* This software is the confidential and proprietary
-* information of iBOXCHAIN Company of China.
-* ("Confidential Information"). You shall not disclose
-* such Confidential Information and shall use it only
-* in accordance with the terms of the contract agreement
-* you entered into with iBOXCHAIN inc.
-*/
+
 
 import F_banana from './F_banana.png'
 import F_batilisk_wing from './F_batilisk_wing.png'
--- src/database/cmd.ts
+++ src/database/cmd.ts
@@ -1,14 +1,4 @@
-/*
-* Copyright (C) 2011-2019 ShenZhen iBOXCHAIN Information Technology Co.,Ltd.
-* All right reserved.
-*
-* This software is the confidential and proprietary
-* information of iBOXCHAIN Company of China.
-* ("Confidential Information"). You shall not disclose
-* such Confidential Information and shall use it only
-* in accordance with the terms of the contract agreement
-* you entered into with iBOXCHAIN inc.
-*/
+
 
 const CMD = [
   { name: '刷物品到包里', cmd: 'c_give(prefab,amount)' },
